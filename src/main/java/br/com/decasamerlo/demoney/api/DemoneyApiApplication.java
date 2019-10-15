d6b6e76c90222a8e01e00be0c42aec81cc6e54b3
Andre Merlo <casagrandamerlo@gmail.com>
2019-10-15 19:53:57 -0300

    criados profiles do Spring

--- src/main/java/br/com/decasamerlo/demoney/api/DemoneyApiApplication.java
+++ src/main/java/br/com/decasamerlo/demoney/api/DemoneyApiApplication.java
@@ -2,8 +2,12 @@ package br.com.decasamerlo.demoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import br.com.decasamerlo.demoney.api.config.property.DemoneyApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DemoneyApiProperty.class)
 public class DemoneyApiApplication {
 
 	public static void main(String[] args) {
