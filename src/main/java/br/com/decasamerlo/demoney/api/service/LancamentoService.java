5353bd6a8857694ce1117656ab2ebd69ad4657d0
Andre Merlo <casagrandamerlo@gmail.com>
2019-10-16 19:43:51 -0300

    criada atualização de lançamentos

--- src/main/java/br/com/decasamerlo/demoney/api/service/LancamentoService.java
+++ src/main/java/br/com/decasamerlo/demoney/api/service/LancamentoService.java
@@ -2,7 +2,9 @@ package br.com.decasamerlo.demoney.api.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.decasamerlo.demoney.api.model.Lancamento;
@@ -16,16 +18,35 @@ public class LancamentoService {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 
 	public Lancamento salvar(Lancamento lancamento) {
+		validarPessoa(lancamento);
+		return lancamentoRepository.save(lancamento);
+	}
+
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
 		Optional<Pessoa> pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
 		if (!pessoa.isPresent() || pessoa.get().isInativo()) {
 			throw new PessoaInexistenteOuInativaException();
 		}
-		return lancamentoRepository.save(lancamento);
+	}
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		return lancamentoRepository.findById(codigo).orElseThrow(() -> new EmptyResultDataAccessException(1));
 	}
 
 }
